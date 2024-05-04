3ac8e525dc68a8461bd72349ebcede1a3be26eb6
Fred Duarte <fduartej@gmail.com>
2024-05-03 21:08:10 -0500

    Agregar Carrito e eliminar

--- Controllers/CarritoController.cs
+++ Controllers/CarritoController.cs
@@ -6,22 +6,87 @@ using System.Threading.Tasks;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.Extensions.Logging;
 using movieappauth.Models;
+using movieappauth.Data;
+using Microsoft.AspNetCore.Identity;
+using Microsoft.EntityFrameworkCore;
+using System.Dynamic;
 
 namespace movieappauth.Controllers
 {
     public class CarritoController : Controller
     {
         private readonly ILogger<CarritoController> _logger;
+        private readonly UserManager<IdentityUser> _userManager;
+        private readonly ApplicationDbContext _context;
 
-        public CarritoController(ILogger<CarritoController> logger)
+        public CarritoController(ILogger<CarritoController> logger,
+        ApplicationDbContext context,
+        UserManager<IdentityUser> userManager)
         {
             _logger = logger;
+            _userManager = userManager;
+            _context = context;
+        }
+
+        public IActionResult IndexUltimoProductoSesion()
+        {
+            var producto  = Util.SessionExtensions.Get<Producto>(HttpContext.Session,"MiUltimoProducto");
+            return View("UltimoProducto",producto);
         }
 
         public IActionResult Index()
         {
-            var producto  = Util.SessionExtensions.Get<Producto>(HttpContext.Session,"MiUltimoProducto");
-            return View("UltimoProducto",producto);
+            var userIDSession = _userManager.GetUserName(User);
+            if(userIDSession == null){
+                ViewData["Message"] = "Por favor debe loguearse antes de agregar un producto";
+                return RedirectToAction("Index","Catalogo");
+            }
+            var items = from o in _context.DataItemCarrito select o;
+            items = items.Include(p => p.Producto).
+                    Where(w => w.UserID.Equals(userIDSession) &&
+                        w.Status.Equals("PENDIENTE"));
+            var itemsCarrito = items.ToList();
+            var total = itemsCarrito.Sum(c => c.Cantidad * c.Precio);
+
+            dynamic model = new ExpandoObject();
+            model.montoTotal = total;
+            model.elementosCarrito = itemsCarrito;
+            return View(model);
+        }
+
+        public async Task<IActionResult> Add(int? id){
+            var userID = _userManager.GetUserName(User);
+            if(userID == null){
+                _logger.LogInformation("No existe usuario");
+                ViewData["Message"] = "Por favor debe loguearse antes de agregar un producto";
+                return RedirectToAction("Index","Catalogo");
+            }else{
+                var producto = await _context.DataProducto.FindAsync(id);
+                Util.SessionExtensions.Set<Producto>(HttpContext.Session,"MiUltimoProducto", producto);
+                Proforma proforma = new Proforma();
+                proforma.Producto = producto;
+                proforma.Precio = producto.Price;
+                proforma.Cantidad = 1;
+                proforma.UserID = userID;
+                _context.Add(proforma);
+                await _context.SaveChangesAsync();
+                ViewData["Message"] = "Se Agrego al carrito";
+                _logger.LogInformation("Se agrego un producto al carrito");
+                return RedirectToAction("Index","Catalogo");
+            }
+        }
+
+        public async Task<IActionResult> Delete(int? id)
+        {
+            if (id == null)
+            {
+                return NotFound();
+            }
+
+            var itemCarrito = await _context.DataItemCarrito.FindAsync(id);
+            _context.DataItemCarrito.Remove(itemCarrito);
+            await _context.SaveChangesAsync();
+            return RedirectToAction(nameof(Index));
         }
 
         [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
